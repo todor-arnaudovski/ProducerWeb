79f0a0e44dbd4ed95f002874785c8d7e2472e141
Todor A <todorarnaudovski@gmail.com>
2023-02-19 12:17:26 +0100

    moved firebase keys to config

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FB_KEY,
-    authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FB_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FB_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FB_APP_ID,
+    apiKey: "AIzaSyA5Jvh9-XI3UNbycKeEpWhbUPWEZMRmjC8",
+    authDomain: "audioplayerapp-f7290.firebaseapp.com",
+    projectId: "audioplayerapp-f7290",
+    storageBucket: "audioplayerapp-f7290.appspot.com",
+    messagingSenderId: "532014483245",
+    appId: "1:532014483245:web:7473dfc626c3d073aab5a4",
 };
 
 const app = initializeApp(firebaseConfig);
